2a31c10f304558d1b4a563521290896232c4fa04
exshak <sbijabhai@gmail.com>
2021-06-25 20:56:11 -0400

    updated instructions for m1

--- install.sh
+++ install.sh
@@ -10,17 +10,20 @@ version=${version:-$(curl -sS https://update.tabnine.com/bundles/version)}
 
 case $(uname -s) in
 "Darwin")
-    platform="apple-darwin"
+    if [ "$(uname -m)" == "arm64" ]; then
+        platform="aarch64-apple-darwin"
+    else
+        platform="$(uname -m)-apple-darwin"
+    fi
     ;;
 "Linux")
-    platform="unknown-linux-musl"
+    platform="$(uname -m)-unknown-linux-musl"
     ;;
 esac
-triple="$(uname -m)-$platform"
 
 # we want the binary to reside inside our plugin's dir
 cd $(dirname $0)
-path=$version/$triple
+path=$version/$platform
 
 curl https://update.tabnine.com/bundles/${path}/TabNine.zip --create-dirs -o binaries/${path}/TabNine.zip
 unzip -o binaries/${path}/TabNine.zip -d binaries/${path}
